3a4263d5fb32ffd2f7101fa59071bf02f0f8b853
Svetlana Nechaeva <snechaev@student.42.us.org>
2019-11-18 17:25:52 -0800

    norm ok

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: snechaev <snechaev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 10:05:37 by snechaev          #+#    #+#             */
-/*   Updated: 2019/11/13 11:26:52 by snechaev         ###   ########.fr       */
+/*   Updated: 2019/11/18 17:25:11 by snechaev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,34 +20,35 @@
 
 typedef struct
 {
-	int         alt_fmt;
-	int         plus;
-	int         minus;
+	intalt_fmt;
+	int			plus;
+	int			minus;
 	int			sps;
-	int         add_0;
-	int         w_fild;
+	int			add_0;
+	int			w_fild;
 	int			is_prec;
-	int         prec;
-	enum mod_l {hh, h, l, ll, L, non} len;
-	char         conv;
-}               t_format;
+	int			prec;
+	enum mod_l	{hh, h, l, ll, L, non} len;
+	char		conv;
+}				t_format;
 
 typedef struct
 {
-	char        *str;
+	char		*str;
 	char		fill_a;
 	char		fill_p;
 	int			pref;
-	int         size_all;
-	int         size_prec;
-	int         neg;
+	int			size_all;
+	int			size_prec;
+	int			neg;
 	int			len;
-}               t_print;
+}				t_print;
 
 int				ft_printf(const char *format, ...);
 void			init_fmt(t_format *fmt);
 void 			init_p(int neg, char *str, t_format *fmt, t_print *p);
 int             arg_parse(const char **arg, t_format *fmt);
+void			modify_fmt(t_format *fmt);
 int             print_args(va_list *va_l, t_format *fmt);
 int             print_str(char *s, t_format *fmt);
 int 			print_char(char c, t_format *fmt);
@@ -58,6 +59,5 @@ int				print_ull(unsigned long long d, t_format *fmt);
 int				print_ll(long long d, t_format *fmt);
 int				print_double(long double f, t_format *fmt);
 int				put_result(int neg, char *str, t_format *fmt);
-
 
 #endif
